4280e2de363cfa5f1d17aefefd125b2c39116dd6
Henry Arasaki <henry_arasaki@hotmail.com>
2022-06-12 19:43:56 -0300

    migrations

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 require("express-async-errors")
-const database = require("./database/sqlite")
+const migrationsRun = require("./database/sqlite/migrations")
 const AppError = require("./utils/AppError")
 
 const express = require("express")
 
 const routes = require('./routes')
+
+migrationsRun()
 
 
 const app = express()
@@ -13,7 +15,7 @@ app.use(express.json())
 
 app.use(routes)
 
-database()
+
 
 app.use((error,request,response,next)=>{
     if(error instanceof AppError){
